8ed307039b74e19677706035c208bcf8b2492bad
yeafla530 <yeafla530@bearun.co.kr>
2022-04-20 02:35:02 +0900

    |add| react downgrade @17.0.8 | 220420 |

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,22 @@
 import '../styles/globals.css'
-
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+// import {Layout} from "./common";
+import Head from 'next/head'
+const App = ({ Component, pageProps }) => {
+  return (
+      <>
+        <Head>
+            <meta charSet="utf-8"/>
+            <meta name="viewport" 
+            content="width=device-width, user-scalable=no, 
+            initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"></meta>
+            <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
+            <title>Soccer App</title>
+        </Head>
+        {/* <Layout> */}
+            <Component {...pageProps} />
+        {/* </Layout> */}
+      </>
+  ) 
 }
 
-export default MyApp
+export default App
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import DataTable from './common/DataTable.js'
 import styles from '../styles/Home.module.css'
 
