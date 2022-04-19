06635f435e05031bc2fe19e9f688c23b75dc9409
yeafla530 <yeafla530@bearun.co.kr>
2022-04-20 03:11:24 +0900

    |mod| 테이블 더미데이터 제거|220420|

--- pages/common/DataTable.js
+++ pages/common/DataTable.js
@@ -4,15 +4,24 @@ import styles from '../../styles/Home.module.css'
 const DataTable = () => {
     return (
         <table className={styles.table}>
-            <th className={styles.th}>서비스아이디</th>
-            <th className={styles.th}>서비스명</th>
-            <th className={styles.th}>서비스URL</th>
-            <th className={styles.th}>서비스요약</th>
-            <th className={styles.th}>대표문의</th>
-            <th className={styles.th}>소관부처명</th>
-            <th className={styles.th}>최종수정일</th>
-            {/* 첫번째 줄 */}
-            <tr className={styles.tr}>
+            <caption>복지서비스 정보</caption>
+            {/* table header */}
+            <thead>
+                <th className={styles.th}>서비스아이디</th>
+                <th className={styles.th}>서비스명</th>
+                <th className={styles.th}>서비스URL</th>
+                <th className={styles.th}>서비스요약</th>
+                <th className={styles.th}>대표문의</th>
+                <th className={styles.th}>소관부처명</th>
+                <th className={styles.th}>최종수정일</th>
+            </thead>
+            {/* table body */}
+            <tbody>
+                <tr className={styles.tr}>
+                    <td colSpan="7">데이터가 존재하지 않습니다</td>
+                </tr>
+            </tbody>
+            {/* <tr className={styles.tr}>
                 <td className={styles.td}>이름1</td>
                 <td className={styles.td}>1</td>
                 <td className={styles.td}>2</td>
@@ -22,7 +31,6 @@ const DataTable = () => {
                 <td className={styles.td}>3</td>
                 
             </tr>
-            {/* 두번째 줄 */}
             <tr className={styles.tr}>
                 <td className={styles.td}>이름2</td>
                 <td className={styles.td}>1</td>
@@ -31,7 +39,7 @@ const DataTable = () => {
                 <td className={styles.td}>3</td>
                 <td className={styles.td}>3</td>
                 <td className={styles.td}>3</td>
-            </tr>
+            </tr> */}
         </table>
     )
 }
