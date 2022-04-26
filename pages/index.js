10b3cca497aea13f19d9c7244a58fa0a045fd78d
yeafla530 <yeafla530@bearun.co.kr>
2022-04-27 01:14:55 +0900

    |add|redux 폴더 추가|220427|

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,22 @@
 import Head from 'next/head'
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
 import DataTable from './table/dataTable.js'
 import Login from './user/login.js'
 import Join from './user/join.js'
 import styles from '../styles/Table.module.css'
+import axios from 'axios'
 
 export default function Home() {
     const [status, setStatus] = useState("login")
-    
+    // 서버 연결 확인
+    useEffect(() => {
+        axios.get("http://localhost:5000/api/now")
+        .then(res => {
+            let data = res.data
+            document.getElementById('timezone').innerHtml = 
+            `<p>현재시간 ${data}</p>`
+        })
+    }, []);
     return (
         // 홈
         <div className={styles.container}>
@@ -16,6 +25,7 @@ export default function Home() {
             </Head>
             <main>
                 <h1>딥브레인 3기 과제 홈페이지</h1>
+                <p id="timezone">현재시간</p>
             </main>
         </div>
     )
